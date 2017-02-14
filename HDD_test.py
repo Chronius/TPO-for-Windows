7cf0d416131c6e4bb42059e8ca4d917160a759fb
Chronius <bulat1717@yandex.ru>
2017-02-14 13:01:14 +0300

    stress test for HDD, SSD, and removable storage

--- HDD_test.py
+++ HDD_test.py
@@ -1,12 +1,32 @@
 import subprocess
 import os
 
-dir_stressdisk = "resources\soft\stressdisk\\"
+diskpart = 'DISKPART /s listdisk.txt'
+format_script = 'diskpart /s outputscript.txt'
 
-cmd = "stressdisk.exe run "
+dir_stressdisk = "resources\soft\stressdisk\\"
+cmd = "stressdisk.exe -duration 1s run " #Длительность задается в формате XXhXXmXXsXXusXXns
+"""
+Full options:
+  -cpuprofile string
+        Write cpu profile to file
+  -duration duration
+        Duration to run test (default 24h0m0s)
+  -logfile string
+        File to write log to set to empty to ignore (default "stressdisk.log")
+  -maxerrors uint
+        Max number of errors to print per file (default 64)
+  -nodirect
+        Don't use O_DIRECT
+  -s int
+        Size of the check files (default 1000000000)
+  -stats duration
+        Interval to print stats (default 1m0s)
+  -statsfile string
+        File to load/store statistics data (default "stressdisk_stats.json")
+"""
 
 disks_to_check = []
-
 
 import string
 from ctypes import windll
@@ -21,12 +41,15 @@ def get_drives():
 
     return drives
 
-if __name__ == '__main__':
+def format_disk():
+    with subprocess.Popen(format_script, stdout=subprocess.PIPE) as f:
+        for x in f.stdout.readlines():
+            print(x.decode("CP866"))
 
+def stressdiskgo(disk_name):
     err_count = 0
     elapsed_t = None
-    print(get_drives())
-    str = dir_stressdisk + cmd + "H:"
+    str = dir_stressdisk + cmd + disk_name+":"
 
     with subprocess.Popen(str, stderr=subprocess.PIPE) as f:
         while f.poll() is None:
@@ -34,14 +57,43 @@ if __name__ == '__main__':
             #print(x)
             if b"Bytes read" in x or b"Reading file" in x \
                     or b"Writing file" in x or b"Bytes written" in x:
-                print(x)
+                print(x.decode("CP866"))
             if b"Errors" in x:
                 err_count = int(x[24:25])
             if b"Elapsed" in x:
                 elapsed_t = x
 
     if not err_count:
-        print("Test OK")
+        print("Test OK\n" + elapsed_t.decode("CP866"))
     else:
-        print("Test failed\nErrors :", err_count, elapsed_t)
+        print("Test failed\nErrors: ", err_count, "\nElapsed time: ", elapsed_t)
+
+if __name__ == '__main__':
+
+    with subprocess.Popen(diskpart, stdout=subprocess.PIPE) as f:
+        for x in f.stdout.readlines():
+            print(x.decode("CP866"))
+
+    disk = input("Select testing disk:\t")
+    print(disk)
+
+    disk_name = input("Enter the name of the drive:\t")
+
+    output = open("outputscript.txt", "w")
+
+    with open("diskscript.txt", "r") as script:
+        for line in script.readlines():
+            if "select disk" in line:
+                line = "select disk " + disk + '\n'
+            if "assign letter=" in line:
+                line = "assign letter=" + disk_name + '\n'
+            output.writelines(line)
+    output.close()
+
+    #uncomment this line if you want to format the drive
+    #format_disk()
+
+    print(get_drives())
+    stressdiskgo(disk_name)
+
 
